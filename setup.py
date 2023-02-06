8c0dc59922eb3f8c12ffa509a1a4753580b36834
Nguyễn Hòa Bình <binhbhgl5@gmail.com>
2023-02-06 21:01:36 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,21 +16,12 @@ def main():
     username = input("What is your Codefun username?\n")
     pwd = input("What is the password?\n")
 
-    filepath = input(
-        "What is the absolute path to the folder containing your code?\n").replace("/", "\\")
-    if (filepath.endswith("\\")):
-        filepath = filepath[:-1]
+    filepath = ".\codefunpython"
 
     lang = input(
         "What is the default submitting language? (C++/Python3/Pascal/NAsm)\n")
 
-    chromedriverpath = input(
-        "What is the path to your chromedriver.exe file? (Type NA if you don't have chromedriver.exe)\n")
-    while chromedriverpath == "NA":
-        print("Redirecting to https://chromedriver.chromium.org/downloads")
-        webbrowser.open_new_tab("https://chromedriver.chromium.org/downloads")
-        chromedriverpath = input(
-            "What is the path to your chromedriver.exe file?\n")
+    chromedriverpath = ".\chromedriver_win32/chromedriver.exe"
     with open(".env", "w") as f:
         f.write(f"CF_USERNAME = {username}\n")
         f.write(f"CF_PASSWORD = {pwd}\n")
